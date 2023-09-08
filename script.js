6d74148f5d66d31a743d0e9e3628a34a27d9c0c8
Hassan Abass <abass.hassan.7798@gmail.com>
2023-09-08 01:16:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,15 +17,6 @@ res.innerHTML = d;
 const show = document.querySelector("#date");
 
 setInterval(() => {
-  const now = new Date();
-  const year = now.getUTCFullYear();
-  const month = now.getUTCMonth() + 1;
-  const day = now.getUTCDate();
-  const hours = now.getUTCHours();
-  const minutes = now.getUTCMinutes();
-  const seconds = now.getUTCSeconds();
-  const milliseconds = now.getUTCMilliseconds();
-
-  show.innerHTML = `${year}-${month}-${day} 
-            ${hours}:${minutes}:${seconds}.${milliseconds}`;
+  const now = new Date().toLocaleTimeString();
+  show.innerHTML = now;
 }, 1000);
